38633ea9f8d55abb388a47106b6b0ed83ce3e4d0
cj <cjlazell@gmail.com>
2018-10-29 18:59:02 -0500

    use unpkg at build

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const fs = require('fs')
 const StyleLintPlugin = require('stylelint-webpack-plugin')
+const packageJson = require('./package.json')
 
 module.exports = {
   productionSourceMap: false,
+
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? `https://unpkg.com/${packageJson.name}@${packageJson.version}/dist/`
+    : '/',
 
   configureWebpack: {
     plugins: [
